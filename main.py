8af1b87ffcae91b22d57c92f1dd28040b9e1621a
Aalam Sultanji <aalam.sultanji@gmail.com>
2026-05-16 03:24:41 +0200

    main, v1

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 ## Author: Aalam Sultanji
-## Last updated: 23-02-2026
+## Last updated: 15-05-2026
 ## Email: aalam.sultanji@gmail.com
 
 '''
@@ -11,14 +11,38 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
-
-
 import os
 import torchvision
 import torchvision.transforms as transforms
-import CNN
-import preprocessing
+from model.CNN import CNN1
+from train_test.train_test import train, evaluate, test
+from data_loading.dataloading import dataloaders
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+'''
+Main block, runs the training and testing of the model, includes the configurations. 
+'''
+
+#Configurations:
+DATADIR = 'dataset/Data'
+BATCH_SIZE = 32
+EPOCHS = 50
+LEARNING_RATE = 0.001
+NUM_CLASSES = 5
+
+if __name__ == "__main__":
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    train_loader, val_loader, test_loader, class_weights, class_names = dataloaders(DATADIR, batch_size=BATCH_SIZE)
+    model = CNN1(num_classes=NUM_CLASSES).to(device)
+    criterion = nn.CrossEntropyLoss(weight=class_weights.to(device))
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+    #training the model
+    model, history = train(model, train_loader, val_loader, criterion, optimizer, device, epochs=EPOCHS)
+
+    #testing the model
+    model.load_state_dict(torch.load('weights/best_model.pth', weights_only=True))
+    results = test(model, test_loader, device, class_names)
+
+
 
 
